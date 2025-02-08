2d9376b1f52bd3cd90fe19fb5ecca3e5d5320766
Aditya Donapati <adityamdonapati@gmail.com>
2025-02-08 09:04:58 +0530

    day 2

---
+++ 07.02.25/p4.java
@@ -0,0 +1,103 @@
+// You are developing an application for a garden management system where each tree 
+// in the garden is represented as a binary tree structure. The system needs to 
+// allow users to plant new trees in a systematic way, ensuring that each tree is 
+// filled level by level.
+
+// A gardener wants to:
+//  - Plant trees based on user input.
+//  - Ensure trees grow in a balanced way by filling nodes level by level.
+//  - Inspect the garden layout by performing an in-order traversal, which helps 
+//    analyze the natural arrangement of trees.
+
+// Your task is to implement a program that:
+//     - Accepts a list of N tree species (as integers).
+//     - Builds a binary tree using level-order insertion.
+//     - Displays the in-order traversal of the tree.
+
+// Input Format:
+// -------------
+// - An integer N representing the number of tree plants.
+// - A space-separated list of N integers representing tree species.
+
+// Output Format:
+// --------------
+// A list of integers, in-order traversal of tree.
+
+
+// Sample Input:
+// -------------
+// 7
+// 1 2 3 4 5 6 7
+
+// Sample Output:
+// --------------
+// 4 2 5 1 6 3 7
+
+
+// Explanation:
+// ------------
+// The tree looks like this:
+
+//         1
+//        / \
+//       2   3
+//      / \  / \
+//     4   5 6  7
+// The in order is : 4 2 5 1 6 3 7
+
+import java.util.*;
+
+class treenode {
+    int data;
+    treenode left, right;
+    treenode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class Solution {
+    treenode root;
+    public treenode buildtree(int n, int[] tree) {
+        if(n==0) return null;
+        treenode root = new treenode(tree[0]);
+        Queue<treenode> q = new LinkedList<>();
+        q.add(root);
+        int i=1;
+        while(!q.isEmpty()&&i<n) {
+            treenode curr = q.poll();
+            if(i<n) {
+                curr.left = new treenode(tree[i]);
+                q.add(curr.left);
+                i++;
+            }
+            if(i<n) {
+                curr.right = new treenode(tree[i]);
+                q.add(curr.right);
+                i++;
+            }
+        }
+        return root;
+    }
+    public static void inorder(treenode root) {
+        if(root==null) return;
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int tree[] = new int[n];
+        for(int i=0;i<n;i++) {
+            tree[i] = sc.nextInt();
+        }
+        sol.root = sol.buildtree(n,tree);
+        sol.inorder(sol.root);
+        
+    }
+}
---
+++ 07.02.25/p5.java
@@ -0,0 +1,102 @@
+// Write a program to construct a binary tree from level-order input, while treating -1 
+// as a placeholder for missing nodes. The program reads input, constructs the tree, 
+// and provides an in-order traversal to verify correctness.
+
+// Input Format:
+// ---------------
+// Space separated integers, level order data (where -1 indiactes null node).
+
+// Output Format:
+// -----------------
+// Print the in-order data of the tree.
+
+
+// Sample Input:
+// ----------------
+// 1 2 3 -1 -1 4 5
+
+// Sample Output:
+// ----------------
+// 2 1 4 3 5
+
+// Explanation:
+// --------------
+//     1
+//    / \
+//   2   3
+//      / \
+//     4   5
+
+
+// Sample Input:
+// ----------------
+// 1 2 3 4 5 6 7
+
+// Sample Output:
+// ----------------
+// 4 2 5 1 6 3 7
+
+// Explanation:
+// --------------
+//         1
+//        / \
+//       2   3
+//      / \  / \
+//     4  5 6  7
+
+import java.util.*;
+
+class treenode {
+    int data;
+    treenode left, right;
+    treenode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class Solution {
+    treenode root;
+    public treenode buildtree(int n, int[] tree) {
+        if (n==0 || tree[0]==-1) return null;
+        treenode root = new treenode(tree[0]);
+        Queue<treenode> q = new LinkedList<>();
+        q.add(root);
+        int i=1;
+        while(!q.isEmpty()&&i<n) {
+            treenode curr = q.poll();
+            if(i<n && tree[i]!=-1) {
+                curr.left = new treenode(tree[i]);
+                q.add(curr.left);
+            }
+            i++;
+            if(i<n && tree[i]!=-1) {
+                curr.right = new treenode(tree[i]);
+                q.add(curr.right);
+            }
+                i++;
+        }
+        return root;
+    }
+    public static void inorder(treenode root) {
+        if(root==null) return;
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int tree[] = new int[n];
+        for(int i=0;i<n;i++) {
+            tree[i] = sc.nextInt();
+        }
+        sol.root = sol.buildtree(n,tree);
+        sol.inorder(sol.root);
+        
+    }
+}
